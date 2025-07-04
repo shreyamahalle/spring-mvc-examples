6263ac88a085913ab5932107c3bf2b633df5187a
shreya mahalle <shreyamahalle2000@gmail.com>
2025-07-04 13:49:07 +0530

    changes added

---
+++ convert-to-spring-boot-no-web.xml/src/main/java/com/shreya/springboot/MyStringBootApp.java
@@ -0,0 +1,11 @@
+package com.shreya.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStringBootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyStringBootApp.class,args);
+    }
+}
